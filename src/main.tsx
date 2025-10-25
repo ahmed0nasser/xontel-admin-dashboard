b303be0f31e146bd6abdca5db427ae54f37a4adc
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-25 21:12:58 +0300

    Update main to use Router and index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Router from "./router/Router.tsx";
+import "./index.css";
+import { UserProvider } from "./context/UserContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  </StrictMode>
+);
