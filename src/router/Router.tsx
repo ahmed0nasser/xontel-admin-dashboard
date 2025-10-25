d1c161d9941b6218455746438fdccb89abde6e1b
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-25 20:56:17 +0300

    Create Router wrapper

---
+++ src/router/Router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Chat from "../pages/Chat";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+    ],
+  },
+]);
+
+const Router = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
