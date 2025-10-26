213da2368a65fa3c428ea0831b51da5e59fd1761
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-27 01:08:22 +0300

    Reimplement Sidebar with fresh design

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { GrPieChart } from "react-icons/gr";
+import { BiChat } from "react-icons/bi";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -7,28 +9,42 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
+  const { pathname } = useLocation();
+  const dashboardColor = pathname === "/dashboard" && "text-brand-blue";
+  const chatColor = pathname === "/chat" && "text-brand-blue";
+
   return (
     <aside
-      className={`bg-charcoal text-soft-gray p-4 h-full transition-all duration-300 ease-in-out ${
-        isOpen ? "w-64" : "w-0 overflow-hidden"
+      className={`text-md font-bold p-2 h-full transition-all duration-300 ease-in-out ${
+        isOpen ? "w-auto" : "w-0 overflow-hidden"
       }`}
     >
       <nav>
-        <ul>
+        <ul className="uppercase text-gray-600/70 mt-10 space-y-12">
           <li>
             <NavLink
               to="/dashboard"
-              className="block py-2 px-4 rounded hover:bg-light-blue"
+              className={`block py-2 px-4 rounded duration-300 hover:text-brand-blue ${dashboardColor}`}
             >
-              Dashboard
+              <div className="ml-5 text-5xl">
+                <GrPieChart />
+              </div>
+              <div className="mt-4 text-xs tracking-widest flex justify-center">
+                Dashboard
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/chat"
-              className="block py-2 px-4 rounded hover:bg-light-blue"
+              className={`block py-2 px-4 rounded duration-300 hover:text-brand-blue ${chatColor}`}
             >
-              Chat
+              <div className="ml-5 text-5xl">
+                <BiChat />
+              </div>
+              <div className="mt-2 text-xs tracking-widest flex justify-center">
+                Chat
+              </div>
             </NavLink>
           </li>
         </ul>
