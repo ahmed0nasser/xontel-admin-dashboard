7a5b6224e216f9667e0b9a8e2111f79712315bd0
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-27 00:58:43 +0300

    Redirect path: / -> /dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      navigate("/dashboard");
+    }
+  });
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -16,13 +24,11 @@ function App() {
       <div className="flex flex-1 relative">
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
         <main
-          className={`flex-1 p-4 overflow-y-auto transition-all duration-300 ease-in-out ${
-            isSidebarOpen ? "ml-64" : "ml-0"
-          }`}
+          className={`flex-1 p-4 overflow-y-auto transition-all duration-300 ease-in-out`}
         >
           <Outlet />
         </main>
-        <button
+        {/* <button
           onClick={toggleSidebar}
           className={`absolute top-4 bg-brand-blue text-white p-2 rounded-full shadow-lg focus:outline-none transition-all duration-300 ease-in-out ${
             isSidebarOpen
@@ -31,7 +37,7 @@ function App() {
           }`}
         >
           {isSidebarOpen ? "<" : ">"}
-        </button>
+        </button> */}
       </div>
     </div>
   );
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -9,10 +9,6 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
-        element: <Dashboard />,
-      },
-      {
         path: "/dashboard",
         element: <Dashboard />,
       },
