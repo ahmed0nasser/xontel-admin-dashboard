7f68d0fa919b766e3d77d11dfb5590a26f0d5605
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-26 21:39:53 +0300

    Change header's background color

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,21 +5,27 @@ const Header: React.FC = () => {
   const { user } = useUser();
 
   return (
-    <header className="bg-charcoal text-soft-gray p-4 flex items-center justify-between w-full">
+    <header className="shadow-md p-4 flex items-center justify-between w-full">
       <div className="flex items-center">
-        <img
-          src="/src/assets/xontel-logo.png"
-          alt="Logo"
-          className="h-8 w-8 mr-2"
-        />
+        <a href="http://www.XonTel.com">
+          <img
+            src="/src/assets/xontel-logo.png"
+            alt="Logo"
+            className="w-24 mr-2"
+          />
+        </a>
       </div>
       <div className="text-center">
-        <h1 className="text-xl font-bold">HR Feedback Admin Panel</h1>
+        <h1 className="text-zinc-800 text-2xl font-bold">
+          HR Feedback Admin Panel
+        </h1>
       </div>
       <div className="flex items-center">
         {user && (
           <>
-            <span className="mr-4">{user.name}</span>
+            <span className="mr-4 font-semibold text-stone-700">
+              {user.name}
+            </span>
             <img
               src={user.picture}
               alt={user.name}
