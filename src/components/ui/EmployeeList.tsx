517c5732ef0e11a6ac3e7803b78e02f2f63c6570
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-28 01:12:28 +0300

    Update search bar

--- src/components/ui/EmployeeList.tsx
+++ src/components/ui/EmployeeList.tsx
@@ -20,27 +20,30 @@ const EmployeeList: React.FC<EmployeeListProps> = ({
   );
 
   return (
-    <div className="bg-soft-gray text-charcoal h-full overflow-y-auto">
-      <div className="w-full flex items-center mb-2">
-        <span className="text-2xl">
-          <CiSearch />
-        </span>
-        <input
-          type="text"
-          placeholder="Search"
-          className="p-2 w-full rounded-md focus:outline-none focus:shadow-md"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+    <div className="text-charcoal h-full overflow-y-auto">
+      <div className="w-full px-2">
+        <div className="px-2 w-full flex items-center mb-4 rounded-3xl bg-slate-200/90 border border-gray-300 hover:border-gray-400 duration-300">
+          <span className="text-2xl">
+            <CiSearch />
+          </span>
+          <input
+            type="text"
+            placeholder="Search"
+            className="p-2 w-full focus:outline-none"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
+
       <div>
         {filteredEmployees.map((employee: Employee) => (
           <div
             key={employee.id}
             className={`flex items-center p-3 border-b border-zinc-400/50 cursor-pointer transition-colors duration-300 ${
               selectedEmployee?.id === employee.id
-                ? "bg-gray-300"
-                : "hover:bg-gray-300/70"
+                ? "bg-brand-blue/80 text-white"
+                : "hover:bg-brand-blue/20"
             }`}
             onClick={() => onSelectEmployee(employee)}
           >
