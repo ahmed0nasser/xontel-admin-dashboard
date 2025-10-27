d9533b6c2f14af86a27c6fd2c4c3457288924b2b
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-28 01:10:52 +0300

    Add some long text for testing

--- src/data/feedback.ts
+++ src/data/feedback.ts
@@ -12,7 +12,7 @@ export const feedbackData: Feedback[] = [
     date: 1678886400000,
     employeeName: "John Doe",
     score: 5,
-    notes: "Great work on the project!",
+    notes: "Great work on the project! t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
   },
   {
     id: "2",
