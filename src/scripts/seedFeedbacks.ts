d3e466b44884583acd9c084dac1031d27e73ee44
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-29 21:42:13 +0300

    Exit the process when script finishes

--- src/scripts/seedFeedbacks.ts
+++ src/scripts/seedFeedbacks.ts
@@ -5,11 +5,15 @@ import { feedbackData } from "../data/feedback";
 const seedFeedbacks = async () => {
   try {
     for (const feedback of feedbackData) {
-      await addDoc(collection(db, "feedbacks"), feedback);
+      await addDoc(collection(db, "feedback"), feedback);
     }
     console.log("Feedbacks seeded successfully!");
+    // @ts-expect-error correct node process exit
+    process.exit(0);  
   } catch (error) {
     console.error("Error seeding feedbacks:", error);
+    // @ts-expect-error correct node process exit
+    process.exit(1);
   }
 };
 
