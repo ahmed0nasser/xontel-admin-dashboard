4976b05d36c5540d7bc72c2c78085fa58e4a6b16
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-11-01 00:47:47 +0200

    Update script to use process.env

--- src/scripts/seedFeedback.ts
+++ src/scripts/seedFeedback.ts
@@ -1,6 +1,25 @@
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../config/firebase";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck   
 import { feedbackData } from "../data/feedback";
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from 'firebase/firestore';
+
+// 🔥 REPLACE with your Firebase config
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const seedFeedback = async () => {
   try {
