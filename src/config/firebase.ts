9972e6d1b2b4122092b719fdabe260104ebea16c
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-10-31 12:37:15 +0200

    Store firebase config secretly

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDnuYE4QWvno04mfLviwK7REkywSir5zo",
-  authDomain: "hr-chatting-app.firebaseapp.com",
-  projectId: "hr-chatting-app",
-  storageBucket: "hr-chatting-app.firebasestorage.app",
-  messagingSenderId: "473817114563",
-  appId: "1:473817114563:web:81f277a5c27a3614d37f76"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp;
